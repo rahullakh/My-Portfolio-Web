5653532fda52aa3f1fa9a9120d2f77a13f0f3d69
Your Name <dharmadudu@gmail.com>
2026-01-04 20:11:09 +0530

    again by optimize canvas

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ loader.load("/assets/Logo/letter-R2.svg", (data) => {
         const isMobile = window.innerWidth < 768;
 
         if (isMobile) {
-            group.position.x = -50;       
+            group.position.x = -30;       
             group.position.y = 60;       
             group.scale.set(0.12, -0.12, 0.12); 
         } else {
